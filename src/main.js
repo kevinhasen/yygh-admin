27ad7d2a97684f2f77b7fea2dd330c67df1d7bdf
kevinhasen <1113949414@qq.com>
2022-02-05 16:13:19 +0800

    医院后台管理

--- src/main.js
+++ src/main.js
@@ -29,15 +29,15 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
+// Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
-// Vue.use(ElementUI)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
-  router,
+  router,  //挂载路由
   store,
   render: h => h(App)
 })
